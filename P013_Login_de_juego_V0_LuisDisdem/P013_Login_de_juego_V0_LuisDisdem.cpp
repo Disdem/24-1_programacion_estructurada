3db0cad787284ca9b0a5727335c4e3eb87c9c515
Disdem <zap1051@amerike.edu.mx>
2023-10-10 12:05:34 -0600

    Practica 13

---
+++ P013_Login_de_juego_V0_LuisDisdem/P013_Login_de_juego_V0_LuisDisdem.cpp
@@ -0,0 +1,43 @@
+// P013_Login_de_juego_V0_LuisDisdem.cpp
+// Luis Disdem 
+// Login en un videojuego con confirmacion de datos 
+
+#include <iostream>
+#include <locale>
+#include <string>
+
+int main()
+{
+    setlocale(LC_ALL, "es_MX.UTF-8");
+    std::string usuario;
+    std::string contrasena;
+
+    std::cout << "*************+Bienvenido a la pagina de inicio+*********** \n";
+    std::cout << "*************+Ingresa tu usuario y contraseña+************* \n";
+    do
+    {
+        std::cout << "********************+Usuario: +******************** \n";
+        std::cin >> usuario;
+        system("cls");
+        std::cout << "*******************+Contraseña: +*******************\n";
+        std::cin >> contrasena;
+        system("cls");
+
+        if (usuario == "Disdem")
+        {
+            if (contrasena == "333491luis")
+            {
+                std::cout << "##########-Bienvenido de nuevo :D-##########\n  ";
+                std::cout << "                                            \n  ";
+            }
+            else
+            {
+                std::cout << "##########+Intentalo nuevamente, la contraseña es incorrecta+##########\n         ";
+            }
+        }
+        else
+        {
+            std::cout << "##########+Intentalo nuevamente, el usuario es incorrecto+##########\n         ";
+        }
+    } while (true);   
+}
