3a1c937edbe9d8e7ce654fa539a5b52e3eeaadfa
Disdem <zap1051@amerike.edu.mx>
2023-10-16 08:44:47 -0600

    Archivo del examen

---
+++ Examen/Examen.cpp
@@ -0,0 +1,21 @@
+// Examen.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    bool condicion = false;
+
+    if (condicion == false)
+    {
+
+        cout << "Hello World!\n";
+
+        condicion = false;
+
+    }
+
+}
