e0da2dc955ead75f7090ed468d959bed2eefc183
Disdem <zap1051@amerike.edu.mx>
2023-10-16 08:54:17 -0600

    Practica examen

---
+++ practica examen/practica examen.cpp
@@ -0,0 +1,27 @@
+// practica examen.cpp :
+// Luis Disdem
+// Revision de codigo del examen
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	bool sintoma1, sintoma2, sintoma3;
+	string respuesta;
+
+	cout << "Tienes fiebre?\n";
+	cin >> respuesta;
+
+	if (respuesta == "si")
+	{
+		cout << "Tienes tos seca?\n";
+		cin >> respuesta;
+		if (respuesta == "si")
+		{
+
+		}
+	}
+}
+
