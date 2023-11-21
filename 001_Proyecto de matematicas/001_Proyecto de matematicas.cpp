f7280f5cd9b6173d6e9f57370f7c06e158077588
Disdem <zap1051@amerike.edu.mx>
2023-11-21 08:56:11 -0600

    Repare

--- 001_Proyecto de matematicas/001_Proyecto de matematicas.cpp
+++ 001_Proyecto de matematicas/001_Proyecto de matematicas.cpp
@@ -1,5 +1,10 @@
+// Luis Disdem
+// Proyecto de matematicas calculadora de probabilidad
+// Calculadora de videojuegos
+
 #include <iostream>
 #include <cmath>
+#include <locale>
 
 //probabilidad simple
 double probabilidadSimple(double eventosFavorables, double eventosPosibles) {
@@ -32,49 +37,85 @@ double probabilidadGeometrica(double probabilidadExito, int numeroIntentos) {
 }
 
 int main() {
-    // Ejemplo de uso
+    setlocale(LC_ALL, "es_MX.UTF-8");
     double eventosFavorables, eventosPosibles, probabilidadA, probabilidadB_dado_A, probabilidadB, probabilidadExito;
     int n, r, numeroIntentos;
+    int metodo;
+    bool seguir = true;
 
-    // Probabilidad simple
-    std::cout << "Ingrese el número de eventos favorables: ";
-    std::cin >> eventosFavorables;
-    std::cout << "Ingrese el número total de eventos posibles: ";
-    std::cin >> eventosPosibles;
-    std::cout << "La probabilidad simple es: " << probabilidadSimple(eventosFavorables, eventosPosibles) << std::endl;
+    std::cout << "\t\tBienvenido a la calculadora de videojuegos.\t\t\n" <<
+        "\t\tEn esta calculadora clacularemos la probabilidad de victoria con datos basados en la teoria, ronadas .etc\t\t\n" <<
+        "\t\tPor seleccione el metodo de calculo. \t\t\n" <<
+        "\t\t1.- Probabilidad simple\t" << "\t2.- Probabilidad de eventos independientes (dado)\t\n" <<
+        "\t\t3.- Probabilidad de eventos independientes\t" << "\t4.- Combinaciones\t\n" <<
+        "\t\t5.- Permutaciones\t" << "\t6.- Probabilidad geométrica\t\n";
+    std::cin >> metodo; 
 
-    // Probabilidad de eventos dependientes
-    std::cout << "\nIngrese la probabilidad de A: ";
+    do
+    {
+    switch (metodo) 
+    {
+    case 1:
+           // Probabilidad simple
+           std::cout << "Ingrese el número de eventos favorables: ";
+           std::cin >> eventosFavorables;
+           std::cout << "Ingrese el número total de eventos posibles: ";
+           std::cin >> eventosPosibles;
+           std::cout << "La probabilidad simple es: " << probabilidadSimple(eventosFavorables, eventosPosibles) << std::endl;
+           break;
+
+    case 2:
+           // Probabilidad de eventos dependientes
+    {  std::cout << "\nIngrese la probabilidad de A: ";
     std::cin >> probabilidadA;
     std::cout << "Ingrese la probabilidad de B dado A: ";
     std::cin >> probabilidadB_dado_A;
-    std::cout << "La probabilidad de eventos dependientes es: " << probabilidadDependiente(probabilidadA, probabilidadB_dado_A) << std::endl;
+    std::cout << "La probabilidad de eventos dependientes es: " << probabilidadDependiente(probabilidadA, probabilidadB_dado_A) << std::endl; }
+           break;
 
-    // Probabilidad de eventos independientes
-    std::cout << "\nIngrese la probabilidad de A: ";
+    case 3:
+           // Probabilidad de eventos independientes
+    {std::cout << "\nIngrese la probabilidad de A: ";
     std::cin >> probabilidadA;
     std::cout << "Ingrese la probabilidad de B: ";
     std::cin >> probabilidadB;
-    std::cout << "La probabilidad de eventos independientes es: " << probabilidadIndependiente(probabilidadA, probabilidadB) << std::endl;
+    std::cout << "La probabilidad de eventos independientes es: " << probabilidadIndependiente(probabilidadA, probabilidadB) << std::endl; }
+           break;
 
-    // Combinaciones
-    std::cout << "\nIngrese el valor de n para combinaciones: ";
+    case 4:
+           // Combinaciones
+    { std::cout << "\nIngrese el valor de n para combinaciones: ";
     std::cin >> n;
     std::cout << "Ingrese el valor de r para combinaciones: ";
     std::cin >> r;
-    std::cout << "Combinaciones de " << n << " choose " << r << " es: " << combinaciones(n, r) << std::endl;
+    std::cout << "Combinaciones de " << n << " choose " << r << " es: " << combinaciones(n, r) << std::endl; }
+           break;
 
-    // Permutaciones
-    std::cout << "\nIngrese el valor de n para permutaciones: ";
+    case 5:
+           // Permutaciones
+    { std::cout << "\nIngrese el valor de n para permutaciones: ";
     std::cin >> n;
     std::cout << "Ingrese el valor de r para permutaciones: ";
     std::cin >> r;
-    std::cout << "Permutaciones de " << n << " P " << r << " es: " << permutaciones(n, r) << std::endl;
+    std::cout << "Permutaciones de " << n << " P " << r << " es: " << permutaciones(n, r) << std::endl; }
+           break;
 
-    // Probabilidad geométrica
-    std::cout << "\nIngrese la probabilidad de éxito en un solo intento: ";
+    case 6:
+           // Probabilidad geométrica
+    {std::cout << "\nIngrese la probabilidad de éxito en un solo intento: ";
     std::cin >> probabilidadExito;
     std::cout << "Ingrese el número total de intentos: ";
     std::cin >> numeroIntentos;
-    std::cout << "La probabilidad geométrica es: " << probabilidadGeometrica(probabilidadExito, numeroIntentos) << std::endl;
+    std::cout << "La probabilidad geométrica es: " << probabilidadGeometrica(probabilidadExito, numeroIntentos) << std::endl; }
+           break;
+
+    default:
+        std::cout << "\t\tTe fuiste muy lejos master ;D\t\t\n";
+        return 0;
+    }
+    std::cout << "Deseas repetir el algoritmo? 0.- NO 1.- SI: ";
+    std::cin >> seguir;
+    system("cls");
+    } while (seguir); {
+    }
 }
