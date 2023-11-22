27b4e7bb769bd7ca283ae4d225c986ad51671118
Disdem <zap1051@amerike.edu.mx>
2023-11-22 08:13:18 -0600

    puras mamadas

---
+++ MathClient/MathClient.cpp
@@ -0,0 +1,9 @@
+// MathClient.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
