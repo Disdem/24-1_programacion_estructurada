6d281b5f429a38af3005e77723820bae50eee35f
Disdem <143419804+Disdem@users.noreply.github.com>
2023-11-27 07:38:32 -0600

    Delete MathClient directory

--- MathClient/MathClient.cpp
+++
@@ -1,9 +0,0 @@
-// MathClient.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
